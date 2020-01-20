a29105022c5ee105b732cde4a7b4fe7bf8bc1242
pilad0hwtts <pilad0hwtts@yandex.ru>
2020-01-20 20:52:42 +0800

    initial commit

---
+++ src/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace src
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {            
+            Console.WriteLine("/***************\\");
+            Console.WriteLine("|Praise the sun!|");
+            Console.WriteLine("\\_______________/");           
+        }
+    }
+}
